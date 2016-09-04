dd77c2f55df34634abb865e2f10cb3065fc5edea
zjhmale <zjhmale@gmail.com>
2016-09-04 20:07:57 +0800

    travis support

---
+++ README.md
@@ -0,0 +1,3 @@
+# Idris for Visual Studio Code
+
+[![Build Status](https://travis-ci.org/zjhmale/vscode-idris.svg?branch=master)](https://travis-ci.org/zjhmale/vscode-idris)
--- test/extension.test.js
+++ test/extension.test.js
@@ -5,13 +5,13 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
-// The module 'assert' provides assertion methods from node
-var assert = require('assert');
+// The module "assert" provides assertion methods from node
+var assert = require("assert");
 
-// You can import and use all API from the 'vscode' module
+// You can import and use all API from the "vscode" module
 // as well as import your extension to test it
-var vscode = require('vscode');
-var myExtension = require('../extension');
+var vscode = require("vscode");
+var myExtension = require("../extension");
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
