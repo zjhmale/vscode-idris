cabcc80656bdda4b23c72b083dc46b782ab1f9b2
zjhmale <zjhmale@gmail.com>
2017-03-19 01:14:20 +0800

    Fixes #16

---
+++ sync.js
@@ -0,0 +1,43 @@
+let CSON = require("cson")
+let request = require('request');
+let chalk = require('chalk');
+let fs = require('fs')
+let join = require('path').join
+
+let getCSONFile = (url, callback) => {
+  request(url, function (error, response, body) {
+    if (error == null && response.statusCode == 200) {
+      console.log(chalk.green.bold(`Downloaded ${url}`))
+      callback(body)
+    } else {
+      console.log(chalk.red.bold(`Download ${url} failed`))
+    }
+  })
+}
+
+let toJSON = (text, name) => {
+  let space = "\t"
+
+  try {
+    let parsed = CSON.parse(text)
+    console.log(chalk.green.bold(`Translated ${name}`))
+    return JSON.stringify(parsed, null, space)
+  } catch (error) {
+    console.log(chalk.red.bold(`Translate ${name} failed`))
+    return null
+  }
+}
+
+let translate = (name) => {
+  let url = `https://raw.githubusercontent.com/idris-hackers/atom-language-idris/master/grammars/language-${name}.cson`
+  getCSONFile(url, (csonFile) => {
+    let jsonFile = toJSON(csonFile, name)
+    if (jsonFile == null) return
+
+    let file = join(__dirname, `/syntaxes/${name}.json`)
+    fs.writeFileSync(file, jsonFile)
+  })
+}
+
+translate("idris")
+translate("ipkg")
