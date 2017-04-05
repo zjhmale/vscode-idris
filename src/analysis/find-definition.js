bf800614a2be5f7e4ffb4ddd2dbf0ccebf673d5c
zjhmale <zjhmale@gmail.com>
2017-04-05 18:46:16 +0800

    Fixes #67

--- src/analysis/find-definition.js
+++ src/analysis/find-definition.js
@@ -3,26 +3,67 @@ const glob = require("glob")
 const commands = require("../idris/commands")
 const common = require('./common')
 
-let findDefinitionInFile = (definition, uri) => {
-  let content = fs.readFileSync(uri).toString()
-  let contents = content.split("\n")
+let findDefinitionForADTFunctions = (contents, definition, uri, moduleName) => {
+  for (let i = 0; i < contents.length; i++) {
+    let former = contents[i - 1] ? contents[i - 1] : ""
+    let latter = contents[i + 1] ? contents[i + 1] : ""
+    let current = contents[i]
+
+    // For the first function in ADT definition
+    if (current.includes(definition) && new RegExp(`data\\s+\\w+\\s+=\\s+${definition}`, "g").test(former + current + latter)) {
+      return {
+        path: uri,
+        module: moduleName,
+        line: i,
+        column: current.indexOf(definition)
+      }
+    }
+
+    // For the rest of functions in ADT definition
+    if (current.includes(definition) && current.includes("|") && !contents.includes("||")) {
+      let start = current.indexOf("|") + 1
+      let end = current.indexOf(definition)
+      if (end > start && current.slice(start, end).trim().length == 0) {
+        return {
+          path: uri,
+          module: moduleName,
+          line: i,
+          column: current.indexOf(definition)
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Normal functions and GADTs functions
+ */
+let findDefinitionForFunctions = (contents, definition, uri, moduleName) => {
   for (let i = 0; i < contents.length; i++) {
     if (contents[i].includes(definition) && contents[i].includes(":")) {
       let start = contents[i].indexOf(definition) + definition.length
       let end = contents[i].indexOf(":")
       if (end > start && contents[i].slice(start, end).trim().length == 0) {
-        let moduleName = common.getModuleName(uri)
-        if (moduleName) {
-          return {
-            path: uri,
-            module: moduleName,
-            line: i,
-            column: contents[i].indexOf(definition)
-          }
+        return {
+          path: uri,
+          module: moduleName,
+          line: i,
+          column: contents[i].indexOf(definition)
         }
       }
     }
   }
+}
+
+let findDefinitionInFile = (definition, uri) => {
+  let moduleName = common.getModuleName(uri)
+  if (!moduleName) return
+
+  let content = fs.readFileSync(uri).toString()
+  let contents = content.split("\n")
+  let funcDef = findDefinitionForFunctions(contents, definition, uri, moduleName)
+  let adtFuncDef = findDefinitionForADTFunctions(contents, definition, uri, moduleName)
+  return funcDef || adtFuncDef
 }
 
 let findDefinitionInFiles = (definition, uri) => {
