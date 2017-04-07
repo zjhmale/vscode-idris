55d82773f83696fa626d8bf623a9d086582acad6
zjhmale <zjhmale@gmail.com>
2017-04-07 12:33:23 +0800

    Fixes #70

--- src/providers/workspaceSymbolProvider.js
+++ src/providers/workspaceSymbolProvider.js
@@ -7,14 +7,95 @@ const Rx = require('rx-lite')
 const HashMap = require('hashmap')
 const _ = require('lodash')
 
-let uniDefList = []
+let uniIdents = []
+
+const excludes = [
+  "if",
+  "then",
+  "else",
+  "do",
+  "let",
+  "in",
+  "dsl",
+  "impossible",
+  "case",
+  "of",
+  "total",
+  "partial",
+  "mutual",
+  "infix",
+  "infixl",
+  "infixr",
+  "constructor",
+  "where",
+  "with",
+  "syntax",
+  "proof",
+  "postulate",
+  "using",
+  "namespace",
+  "rewrite",
+  "public",
+  "private",
+  "export",
+  "implicit",
+  "module",
+  "import",
+  "auto",
+  "default",
+  "data",
+  "codata",
+  "class",
+  "instance",
+  "interface",
+  "implementation",
+  "record",
+  "Type",
+  "Int",
+  "Nat",
+  "Integer",
+  "Float",
+  "Char",
+  "String",
+  "Ptr",
+  "Bits8",
+  "Bits16",
+  "Bits32",
+  "Bits64",
+  "Bool",
+  "_",
+  "True",
+  "False",
+  "Just",
+  "Nothing"
+]
 
 let buildSymbolSearchList = () => {
   let files = common.getAllFiles('idr')
   let defList = files.map((uri) => {
-    return common.getIdentList(uri).map((name) => {
-      let def = findDefinition.findDefinitionInFiles(name, uri)
-      if (def) {
+    return common.getIdentList(uri).filter((name) => {
+      return !/\b\d+\b/g.test(name) && !excludes.includes(name)
+    }).map((name) => {
+      return { name, uri }
+    })
+  })
+  uniIdents = _.uniqWith(_.flatten(defList), _.isEqual);
+}
+
+let IdrisWorkspaceSymbolProvider = (function () {
+  function IdrisWorkspaceSymbolProvider() { }
+
+  IdrisWorkspaceSymbolProvider.prototype.provideWorkspaceSymbols = function (query, token) {
+    return new Promise((resolve, reject) => {
+      buildSymbolSearchList()
+      let defs = uniIdents.filter(({ name, _ }) => {
+        return query != "" && (name.toLowerCase().includes(query) || name.includes(query))
+      }).map(({ name, uri }) => {
+        let def = findDefinition.findDefinitionInFiles(name, uri)
+        return { name, def }
+      }).filter((i) => i.def != undefined)
+      let uniDefs = _.uniqWith(_.flatten(defs), _.isEqual);
+      let symbols = uniDefs.map(({ name, def }) => {
         let pos = new vscode.Position(def.line, def.column);
         let range = new vscode.Range(pos, pos);
         let info = new vscode.SymbolInformation(
@@ -24,29 +105,8 @@ let buildSymbolSearchList = () => {
           vscode.Uri.file(def.path),
           ""
         )
-        return { name, info }
-      } else {
-        return null
-      }
-    }).filter((i) => i != null)
-  })
-  uniDefList = _.uniqWith(_.flatten(defList), _.isEqual);
-}
-
-let IdrisWorkspaceSymbolProvider = (function () {
-  function IdrisWorkspaceSymbolProvider() { }
-
-  IdrisWorkspaceSymbolProvider.prototype.provideWorkspaceSymbols = function (query, token) {
-    return new Promise((resolve, reject) => {
-      buildSymbolSearchList()
-      let symbols = uniDefList.map((def) => {
-        if (def.name.toLowerCase().startsWith(query)
-          || def.name.startsWith(query)) {
-          return def.info
-        } else {
-          return null
-        }
-      }).filter((info) => info != null)
+        return info
+      })
       resolve(symbols)
     }).then(function (symbols) {
       return symbols
