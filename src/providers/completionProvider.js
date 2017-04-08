22c7a3af2f520021b53e04b66d671dcde32ca1e6
zjhmale <zjhmale@gmail.com>
2017-04-08 16:21:13 +0800

    Fixes #61

--- src/providers/completionProvider.js
+++ src/providers/completionProvider.js
@@ -3,6 +3,7 @@ const commands = require('../idris/commands')
 const controller = require('../controller')
 const vscode = require('vscode')
 const common = require('../analysis/common')
+const snippets = require('../../snippets/idris.json')
 
 let identList
 
@@ -14,6 +15,14 @@ let buildCompletionList = () => {
 let IdrisCompletionProvider = (function () {
   function IdrisCompletionProvider() { }
 
+  let snippetItems = Object.keys(snippets).map((k) => {
+    let snippet = snippets[k]
+    let item = new vscode.CompletionItem(snippet.prefix)
+    item.insertText = new vscode.SnippetString(snippet.body.join("\n"))
+    item.kind = 14
+    return item
+  })
+
   IdrisCompletionProvider.prototype.provideCompletionItems = (document, position, token) => {
     let wordRange = document.getWordRangeAtPosition(position)
     let currentWord = document.getText(wordRange)
@@ -24,11 +33,12 @@ let IdrisCompletionProvider = (function () {
       let suggestMode = vscode.workspace.getConfiguration('idris').get('suggestMode')
 
       if (suggestMode == 'allWords') {
-        return identList.filter((ident) => {
+        let suggestionItems = identList.filter((ident) => {
           return ident.startsWith(trimmedPrefix) || ident.toLowerCase().startsWith(trimmedPrefix)
         }).map((ident) => {
           return new vscode.CompletionItem(ident, 0)
         })
+        return suggestionItems.concat(snippetItems)
       } else if (suggestMode == 'replCompletion') {
         return controller.getCompilerOptsPromise().flatMap((compilerOptions) => {
           commands.initialize(compilerOptions)
@@ -42,7 +52,7 @@ let IdrisCompletionProvider = (function () {
           let results = ref.map((v, i, arr) => {
             return new vscode.CompletionItem(v, 1)
           })
-          return results
+          return results.concat(snippetItems)
         })
       } else {
         vscode.window.showErrorMessage("Invalid option for idris.suggestMode")
