294cbde39863a9e34fe782d26782d919e1741e5b
zjhmale <zjhmale@gmail.com>
2017-05-01 01:28:44 +0800

    Documenthighlights for local symbols

--- src/providers/idris/documentHighlightProvider.js
+++ src/providers/idris/documentHighlightProvider.js
@@ -14,31 +14,40 @@ let IdrisDocumentHighlightProvider = (function () {
     let uri = document.uri.fsPath
     let currentWordDef = findDefinition.findDefinitionInFiles(currentWord, uri)
 
-    return new Promise((resolve, _reject) => {
-      let uniIdents = common.getIdents(uri)
-      let positions = uniIdents.filter((name) => {
-        if (name == currentWord) {
-          let def = findDefinition.findDefinitionInFiles(name, uri)
-          if (def == undefined) {
-            return false
+    if (currentWordDef != undefined) {
+      return new Promise((resolve, _reject) => {
+        let uniIdents = common.getIdents(uri)
+        let positions = uniIdents.filter((name) => {
+          if (name == currentWord) {
+            let def = findDefinition.findDefinitionInFiles(name, uri)
+            if (def == undefined) {
+              return false
+            } else {
+              return common.isDefinitonEqual(currentWordDef, def)
+            }
           } else {
-            return common.isDefinitonEqual(currentWordDef, def)
+            return false
           }
-        } else {
-          return false
-        }
-      }).map((name) => {
-        return common.getAllPositions(name, uri)
+        }).map((name) => {
+          return common.getAllPositions(name, uri)
+        })
+        let uniPositions = _.uniqWith(_.flatten(positions), _.isEqual)
+        let highlights = uniPositions.map(({ _uri, line, column }) => {
+          let startPos = new vscode.Position(line, column)
+          let endPos = new vscode.Position(line, column + currentWord.length)
+          let range = new vscode.Range(startPos, endPos)
+          return new vscode.DocumentHighlight(range, vscode.DocumentHighlightKind.Text)
+        })
+        resolve(highlights)
       })
-      let uniPositions = _.uniqWith(_.flatten(positions), _.isEqual)
-      let highlights = uniPositions.map(({ uri, line, column }) => {
+    } else {
+      return common.getAllPositions(currentWord, uri).map(({ _uri, line, column }) => {
         let startPos = new vscode.Position(line, column)
         let endPos = new vscode.Position(line, column + currentWord.length)
         let range = new vscode.Range(startPos, endPos)
         return new vscode.DocumentHighlight(range, vscode.DocumentHighlightKind.Text)
       })
-      resolve(highlights)
-    })
+    }
   }
   return IdrisDocumentHighlightProvider
 }())
